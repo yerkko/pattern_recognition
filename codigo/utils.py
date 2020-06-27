09be4da42eefda866f88f59c30e3df219ec11702
Cristóbal Espinoza <caespinoza5@uc.cl>
2020-06-27 11:26:13 -0400

    added sfs, pca

--- codigo/utils.py
+++ codigo/utils.py
@@ -4,22 +4,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+from pybalu.feature_transformation import pca
+from pybalu.feature_selection import sfs
 BASE_FOLDER = 'FaceMask166'
+
 
 def load_image(image_name, gray=False, crop=False, base_folder=BASE_FOLDER):
     """Carga una imagen con open-cv."""
     if not image_name.endswith('.jpg'):
         image_name = f'{image_name}.jpg'
-    
+
     image_path = os.path.join(base_folder, image_name)
     image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    
+
     if image is None:
         raise Exception(f'Cant load image: {image_name}')
-    
+
     # open-cv loads as BGR
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
+
     if gray:
         image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
@@ -31,7 +34,7 @@ def load_image(image_name, gray=False, crop=False, base_folder=BASE_FOLDER):
 
 def load_dataset(n_images, gray=True, crop=True):
     """Carga un dataset de las primeras N imagenes.
-    
+
     Returns:
         dataset lista de: [image_cropped, image_name, i_image (label), j (image_number)]
     """
@@ -44,21 +47,22 @@ def load_dataset(n_images, gray=True, crop=True):
             # Load image
             image_name = f'{base_name}_{j:02d}'
             image = load_image(image_name, gray=gray, crop=crop)
-            
+
             dataset.append((image, image_name, i_image, j))
-            
+
     return dataset
+
 
 def create_df(dataset, merge=None):
     """Crea un dataframe a partir de un dataset.
-    
+
     Args:
         dataset -- formato que retorna `load_dataset()`
     """
     data = [sample[2:] for sample in dataset]
     image_names = [sample[1] for sample in dataset]
     df = pd.DataFrame(data, index=image_names, columns=['label', 'image_number'])
-    
+
     if merge is not None:
         df = df.merge(merge, right_index=True, left_index=True)
 
@@ -67,7 +71,7 @@ def create_df(dataset, merge=None):
 
 def calculate_feature_df(dataset, feature_fn, feature_name, **kwargs):
     """Calcula un feature con `feature_fn()` para todas las imagenes de un dataset, retorna un DF.
-    
+
     Args:
         dataset -- formato que retorna `load_dataset()`
         feature_fn -- funcion de firma `features = feature_fn(image, **kwargs)`
@@ -77,7 +81,7 @@ def calculate_feature_df(dataset, feature_fn, feature_name, **kwargs):
     print(f'Calculating {feature_name}')
     image_names = []
     features = []
-    
+
     # Cargar imagenes y calcular vectores de features
     for sample in dataset:
         image = sample[0]
@@ -85,23 +89,23 @@ def calculate_feature_df(dataset, feature_fn, feature_name, **kwargs):
 
         image_features = feature_fn(image, **kwargs)
         features.append(image_features)
-    
+
     features = np.array(features)
     n_images, n_feats = features.shape
 
     # Nombres de los features, e.g. 'hog_0', 'hog_1', etc
     columns = [f'{feature_name}_{idx}' for idx in range(n_feats)]
-    
+
     # Crear DataFrame, el index son nombres de las imagenes
     feature_df = pd.DataFrame(features, index=image_names, columns=columns)
-    
+
     # Retornar DataFrame con features calculados
     return feature_df
 
 
 def plot_cm(cm, title='', labels=None, colorbar=False):
     """Plotear una matriz de confusion.
-    
+
     Args:
         cm -- matriz de n_classes x n_classes
         title -- string para el titulo
@@ -110,7 +114,7 @@ def plot_cm(cm, title='', labels=None, colorbar=False):
     """
     n_labels = len(cm)
     ticks = np.arange(n_labels)
-    
+
     if labels is None:
         labels = ticks
 
@@ -138,7 +142,7 @@ def plot_cm(cm, title='', labels=None, colorbar=False):
 
 def normalize_df(df, keep_nonorm=True, ignore=['label', 'image_number']):
     """Normaliza los datos de un DataFrame.
-    
+
     Args:
         df -- DataFrame con shape (n_imagenes, n_features)
         keep_nonorm -- Si es True, devuelve el dataframe incluyendo las columnas
@@ -161,7 +165,6 @@ def normalize_df(df, keep_nonorm=True, ignore=['label', 'image_number']):
     data_norm = normalization.transform(df[feature_columns])
     df_norm = pd.DataFrame(data_norm, index=df.index, columns=feature_columns)
 
-
     # Keep both columns (normalized and not normalized)
     if keep_nonorm:
         # Rename no-normalized to '<col>_nonorm'
@@ -173,3 +176,28 @@ def normalize_df(df, keep_nonorm=True, ignore=['label', 'image_number']):
 
     # Add the ignored columns from the original dataset
     return df_norm.merge(df[ignore], how='inner', right_index=True, left_index=True)
+
+
+def PCA(x_train, x_test, x_val, n_components):
+    """
+    Realiza la transformación PCA de los datos a tan solo 'n_components' características.
+    n_components:   número de características.
+    """
+    x_train, _, A, Xm, _ = pca(x_train, n_components=n_components)
+    x_test = np.matmul(x_test - Xm, A)
+    x_val = np.matmul(x_val - Xm, A)
+    return x_train, x_test, x_val
+
+
+def SFS(x_train, y_train, x_test, x_val, n_features, method="fisher", show=False):
+    """
+    Realiza la selección de 'n_features' características secuencialmente.
+    n_features:     número de características
+    method:         método a ocupar en la selección
+    show:           mostrar el progreso de la selección.
+    """
+    s_sfs = sfs(x_train, y_train, n_features=n_features, method="fisher", show=show)
+    x_train = x_train[:, s_sfs]
+    x_test = x_test[:, s_sfs]
+    x_val = x_val[:, s_sfs]
+    return x_train, x_test, x_val
